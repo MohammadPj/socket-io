adf1c7f23fbcbd59004d1d52ca02735c54802481
Muhammad <mohammadpoorjamal@gmail.com>
2022-10-11 16:45:40 +0330

    chat application

--- server/index.js
+++ server/index.js
@@ -9,13 +9,26 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3002",
         methods: ["GET", "POST"]
     }
 })
 
-const PORT = 3001
+io.on("connection", (socket) => {
+    console.log(`user connected: ${socket.id}`)
 
+    socket.on("join_room", (data) => {
+        socket.join(data)
+    })
+
+    socket.on("send_message", (data) => {
+        console.log("data", data)
+        // socket.broadcast.emit("receive_message", data)
+        socket.to(data.room).emit("receive_message", data)
+    })
+})
+
+const PORT = 3001
 server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
